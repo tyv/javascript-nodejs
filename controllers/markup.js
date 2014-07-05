dfe11800424fb3925ebfbcf60d1a6ff22130c391
Shuvalov Anton <anton_shuvalov@me.com>
2014-07-05 21:10:52 +0400

    add basic jade structure

--- controllers/markup.js
+++ controllers/markup.js
@@ -2,7 +2,8 @@ var join = require('path').join;
 
 exports.get = function *get (next) {
   var path = join('markup', this.params[0]);
-
+  console.log(this.params);
+  console.log(this.params[0]);
   yield this.render(path);
 };
 
