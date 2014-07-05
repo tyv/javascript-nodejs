2f003c93523f1615fb2eb4b27bc149686efa3d8b
Shuvalov Anton <anton_shuvalov@me.com>
2014-07-06 03:52:17 +0400

    fix bug with slowpoke stylus add require-tree to require controllers restore serve('www') for static. Now the stylus mw send files by itself.

--- controllers/stylus.js
+++ controllers/stylus.js
@@ -1,7 +1,11 @@
 var stylus = require('stylus');
+var send = require('koa-send');
 
 exports.get = function *(next) {
+  this.root = process.cwd() + '/app';
   yield stylus
-    .middleware(process.cwd() + '/app')
-    .bind(null, this.req, this.res)(next);
+    .middleware(this.root)
+    .bind(null, this.req, this.res);
+  yield next;
+  yield send(this, this.path, {root: this.root});
 };
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,18 @@
 
 var log = require('lib/log')(module);
 var config = require('config');
+var requireTree = require('require-tree');
+var controllers = requireTree('../controllers');
 
 // Первым делом идут роуты для схем с жёстким началом /markup/* /task/*  и т.п.
 // В конце идут роуты для статей и справочников, которые в корне сайта, например http://javascript.ru/String
 // TODO: добавить обработку 404 Not Found, сделать возможность любому роуту легко вызывать ошибку 404, как и любую другую ошибку
 module.exports = function(app) {
 
-  app.get('/', require('controllers/frontpage').get);
-
-  app.get('/stylesheets/main.css', require('controllers/stylus').get);
+  app.get('/', controllers.frontpage.get);
+  app.get('/stylesheets/main.css', controllers.stylus.get);
 
   if (process.env.NODE_ENV == 'development') {
-    app.get('/markup/*', require('controllers/markup').get);
+    app.get('/markup/*', controllers.markup.get);
   }
 };
