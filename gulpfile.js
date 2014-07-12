d8080cedb9c139d0a225da683a047618da947568
Ilya Kantor <iliakan@gmail.com>
2014-07-12 13:50:53 +0400

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,10 @@ const mongoose = require('lib/mongoose');
 const execSync = require('child_process').execSync;
 
 gulp.task('lint', function(callback) {
-  const files = execSync("git ls-files -m '*.js'", {encoding: 'utf-8'}).trim().split("\n");
-  console.log("!!!", files);
+  const files = execSync("git ls-files -m '*.js'", {encoding: 'utf-8'})
+    .trim()
+    .split("\n")
+    .filter(function(s) { return s; });
   gulpTaskLint(files)(function() {
     mongoose.disconnect();
     callback.apply(null, arguments);
