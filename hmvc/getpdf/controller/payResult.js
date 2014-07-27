2d6da51c68b4d2e1f7294898937da3453fcb708f
Ilya Kantor <iliakan@gmail.com>
2014-07-27 16:05:07 +0400

    pause tutorial pdf, it's obvous now, move to more complex stuff

--- hmvc/getpdf/controller/payResult.js
+++ hmvc/getpdf/controller/payResult.js
@@ -2,7 +2,15 @@ const payments = require('payments');
 var Order = payments.Order;
 var OrderTemplate = payments.OrderTemplate;
 var Transaction = payments.Transaction;
+const escapeHtml = require('escape-html');
 
+/**
+ * 3 kinds of response
+ * 1) { status, successHtml } - if order success
+ * 2) { status, statusMessage(optional) } - if another status
+ * 3) "" - empty string if no information
+ * @param next
+ */
 exports.get = function*(next) {
   this.set('Cache-Control', 'no-cache, no-store, must-revalidate');
 
@@ -20,33 +28,52 @@ exports.get = function*(next) {
 
   var lastTransaction = yield Transaction.findOne({ order: this.order._id }).sort({created: -1}).exec();
 
-  // no payment at all? strange
+  // no payment at all?!? strange
   if (!lastTransaction) {
     this.body = {
       status: Transaction.STATUS_FAIL,
-      html:   'Оплаты не было.'
+      html: 'Оплаты не было.'
     };
     return;
   }
 
-  // last transaction was successful, but the order is not
+  // the order is not yet successful, but the last transaction is not,
+  // that's possible if order.onSuccess hook has not yet finished
   // let's wait a little bit
   if (lastTransaction.status == Transaction.STATUS_SUCCESS) {
     this.body = '';
-    this.status = 204; // no content
     return;
   }
 
-  // transaction status unknown -> waiting
+  // transaction status unknown
+  //  -> it means we're awaiting a response from the payment system
   if (!lastTransaction.status) {
     this.body = '';
-    this.status = 204; // no content
     return;
   }
 
-  this.body = {
-    status: lastTransaction.status,
-    statusMessage:   lastTransaction.statusMessage
-  };
+  if (lastTransaction.status == Transaction.STATUS_FAIL) {
+    this.body = {
+      status: lastTransaction.status,
+      html:   'Оплата не прошла.'
+    };
+
+    if (lastTransaction.statusMessage) {
+      this.body.html += '<div>' + escapeHtml(lastTransaction.statusMessage) + '</div>';
+    }
+    return;
+  }
+
+
+  if (lastTransaction.status == Transaction.STATUS_PENDING) {
+    this.body = {
+      status: lastTransaction.status,
+      html:   'Оплата ожидается.'
+    };
+
+    if (lastTransaction.statusMessage) {
+      this.body.html += '<div>' + escapeHtml(lastTransaction.statusMessage) + '</div>';
+    }
+  }
 
 };
