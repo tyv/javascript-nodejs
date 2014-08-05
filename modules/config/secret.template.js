c412f15e82ac72c835789c50b8d3f3ed3be0902e
Ilya Kantor <iliakan@gmail.com>
2014-08-05 23:40:42 +0400

    secret

--- modules/config/secret.template.js
+++ modules/config/secret.template.js
@@ -10,14 +10,24 @@ exports.payments = {
 
 exports.auth = {
   facebook:  {
+    appId:     '*',
+    appSecret: '*'
   },
   google:    {
+    appId:     '*',
+    appSecret: '*'
   },
   github:    {
+    appId:     '*',
+    appSecret: '*'
   },
   yandex:    {
+    appId:     '*',
+    appSecret: '*'
   },
   vkontakte: {
+    appId:     '*',
+    appSecret: '*'
   }
 };
 exports.ses = {};
