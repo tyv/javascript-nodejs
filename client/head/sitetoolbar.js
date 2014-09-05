2975030e8432744a614085dd0d31be55e7355252
Ilya Kantor <iliakan@gmail.com>
2014-09-05 12:45:37 +0400

    static & meta

--- client/head/sitetoolbar.js
+++ client/head/sitetoolbar.js
@@ -4,18 +4,24 @@ var lastPageYOffset = window.pageYOffset;
 var ignoreJump = false;
 
 window.addEventListener('scroll', function() {
+  console.log(window.pageYOffset);
   if (ignoreJump) return;
-  if (window.pageYOffset > lastPageYOffset) {
-    // scrolled down, hide nav
-    document.body.classList.add('scrolled-out');
-  } else {
-    // scrolled up
-    document.body.classList.remove('scrolled-out');
-  }
-  lastPageYOffset = window.pageYOffset;
+ // setTimeout(function() {
+    if (window.pageYOffset < lastPageYOffset || window.pageYOffset < 5) {
+      console.log("UP");
+      // scrolled up
+      document.body.classList.remove('scrolled-out');
+    } else if (window.pageYOffset > lastPageYOffset + 30) {
+      console.log("DOWN");
+      // scrolled down, hide nav
+      document.body.classList.add('scrolled-out');
+    }
+
+    lastPageYOffset = window.pageYOffset;
+ // }, 100);
 });
 
-// don't autoscroll when a click on a navigation header
+// don't autoscroll after a click on a navigation header
 document.addEventListener('click', function() {
   ignoreJump = true;
   setTimeout(function() {
