74a9d8e6be3ce5648bfa7a34990971152401769c
Ilya Kantor <iliakan@gmail.com>
2014-10-13 02:39:03 +0400

    better error handling w/ webpack

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var gp = require('gulp-load-plugins')();
+var notifier = require('node-notifier');
 
 module.exports = function() {
 
@@ -7,8 +8,19 @@ module.exports = function() {
 
     var config = require('config/webpack');
     webpack(config, function(err, stats) {
+      if (!err) {
+        var jsonStats = stats.toJson();
+        err = jsonStats.errors[0] || jsonStats.warnings[0];
+      }
+
       if (err) {
-        throw new gp.util.PluginError('webpack', err);
+
+        notifier.notify({
+          message: err
+        });
+
+        gp.util.log(err.message);
+
       }
 
       gp.util.log('[webpack]', stats.toString({
