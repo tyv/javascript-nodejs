b3cfbe1905fb26413f960160f573377ef610666a
Ilya Kantor <iliakan@gmail.com>
2014-10-25 18:17:02 +0400

    minor

--- hmvc/tutorial/tasks/importWatch.js
+++ hmvc/tutorial/tasks/importWatch.js
@@ -13,7 +13,7 @@ module.exports = function(options) {
     var importer = new Importer({
       root:     root,
       onchange: function(path) {
-        livereload.changed(path);
+        liverelpoad.changed(path);
       }
     });
     watch(root, function(filePath, flags, id) {
