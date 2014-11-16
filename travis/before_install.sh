d558b5861663aa8d3c8b26706e518b48198ae324
Ilya Kantor <iliakan@gmail.com>
2014-11-16 05:09:58 +0300

    travis fixes

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,10 +2,12 @@
 
 # echo -e 'travis_fold:start:Log'
 
+# add credentials to .netrc for private repo access
+# travis env set CI_USER_TOKEN [github API token] --private -r iliakan/javascript-nodejs
 
 echo -e "machine github.com\nlogin $CI_USER_TOKEN\nmachine api.github.com\nlogin $CI_USER_TOKEN" >> ~/.netrc
 
-git submodule update --init --recursive
+git submodule update --init --remote
 
 # need latest npm (less bugs, at time of writing 2.0.0 didn't work)
 npm i -g npm
