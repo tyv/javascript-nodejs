f797eb716d05f4608fcdf7136f6009056e19bd9d
Ilya Kantor <iliakan@gmail.com>
2014-11-19 12:51:54 +0300

    refactory deploy scripts

--- pm2/pre_deploy.sh
+++ pm2/pre_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-chmod +x ./pm2/*.sh
-
 #   rm -rf node_modules && npm i && find node_modules -name .git -delete && git add --force node_modules
 git pull origin master &&
   NODE_ENV=production node --harmony `which gulp` build &&
