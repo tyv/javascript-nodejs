d74314bd014e13b9c9b24016d6b4ae8a0faa9d02
Ilya Kantor <iliakan@gmail.com>
2014-11-19 12:26:37 +0300

    better redeploy handling

--- update_prod.sh
+++ update_prod.sh
@@ -4,5 +4,5 @@
 git pull origin master &&
   NODE_ENV=production node --harmony `which gulp` build &&
   git add --force public manifest &&
-  git commit -a -m "update node_modules & rebuild" &&
-  git push origin production
+  # git push gives exit code 1 if nothing is pushed, so I push only if changes exist
+  (git diff-index --quiet HEAD || git commit -a -m deploy && git push origin production)
