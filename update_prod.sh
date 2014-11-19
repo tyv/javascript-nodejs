ed10dc701400dac92aa06ddc7c86be882dfbb1fa
Ilya Kantor <iliakan@gmail.com>
2014-11-19 12:24:31 +0300

    deploy

--- update_prod.sh
+++ update_prod.sh
@@ -4,5 +4,4 @@
 git pull origin master &&
   NODE_ENV=production node --harmony `which gulp` build &&
   git add --force public manifest &&
-  git commit -a -m "update node_modules & rebuild" &&
-  git push origin production
+  (git diff-index --quiet HEAD || git commit -a -m deploy && git push origin production)
