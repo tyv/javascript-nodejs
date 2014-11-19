a1bcab1a15df7052ac34076fa3b0fd0da456f8e2
Ilya Kantor <iliakan@gmail.com>
2014-11-19 23:19:53 +0300

    fix

--- travis/install.sh
+++ travis/install.sh
@@ -35,10 +35,6 @@ chmod 600 ~/.ssh/authorized_keys
 
 ssh -fnNR 2222:localhost:22 travis@stage.javascript.ru
 
-ls /etc
-ls /etc/ssh
-cat /etc/ssh/*
-
 # ==== Install nginx =======
 sudo apt-get install nginx
 
