34254aed777c64c257d85b2740a4a9aea82ce3db
Ilya Kantor <iliakan@gmail.com>
2014-11-29 23:50:24 +0300

    fix nginx root

--- pm2/post_deploy.sh
+++ pm2/post_deploy.sh
@@ -6,5 +6,5 @@
 
 /root/.nvm/v0.11.14/bin/pm2 startOrGracefulReload ecosystem.json --env production &&
  ./gulp cache:clean &&
- ./gulp config:nginx --prefix /etc/nginx --env production &&
+ ./gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs/current &&
  /etc/init.d/nginx reload
