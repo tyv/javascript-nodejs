0bece381f8cc5788c154464e3ffd1fc071d68878
Ilya Kantor <iliakan@gmail.com>
2014-12-09 11:12:52 +0300

    deploy fixes

--- client/notify.js
+++ client/notify.js
@@ -1,6 +1,6 @@
 var humane = require('humane-js');
 
-window.info = exports.info = humane.spawn({ addnCls: 'humane-libnotify-info', timeout: 3000 });
-window.success = exports.success = humane.spawn({ addnCls: 'humane-libnotify-success', timeout: 3000 });
-window.warning = exports.warning = humane.spawn({ addnCls: 'humane-libnotify-warning', timeout: 3000 });
-window.error = exports.error = humane.spawn({ addnCls: 'humane-libnotify-error', timeout: 3000 });
+window.info = exports.info = humane.spawn({addnCls: 'humane-libnotify-info', timeout: 3000});
+window.success = exports.success = humane.spawn({addnCls: 'humane-libnotify-success', timeout: 3000});
+window.warning = exports.warning = humane.spawn({addnCls: 'humane-libnotify-warning', timeout: 3000});
+window.error = exports.error = humane.spawn({addnCls: 'humane-libnotify-error', timeout: 3000});
