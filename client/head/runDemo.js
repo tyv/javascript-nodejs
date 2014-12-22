6d468fd0f62d564eb2a150684e67548c0b95fc59
Ilya Kantor <iliakan@gmail.com>
2014-12-22 19:05:37 +0300

    new mini-layout & password recovery

--- client/head/runDemo.js
+++ client/head/runDemo.js
@@ -3,15 +3,11 @@ window.runDemo = function(button) {
   var demoElem;
   var parent = button;
 
-  debugger;
-
   /* jshint -W084 */
   while(parent = parent.parentElement) {
     demoElem = parent.querySelector('[data-demo]');
     if (demoElem) break;
   }
-
-
 
   if (!demoElem) {
     alert("Ошибка, нет элемента с демо");
