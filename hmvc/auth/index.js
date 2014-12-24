7841354e629ee37660cbe1c0be9c8bae3a9d7cee
Ilya Kantor <iliakan@gmail.com>
2014-12-24 14:47:42 +0300

    aboutme works now

--- hmvc/auth/index.js
+++ hmvc/auth/index.js
@@ -3,16 +3,3 @@ exports.mustNotBeAuthenticated = require('./lib/mustNotBeAuthenticated');
 exports.mustBeAdmin = require('./lib/mustBeAdmin');
 
 exports.middleware = require('lib/lazyRouter')('./router');
-
-/*
-
- var middleware = null;
-
- exports.middleware = function*(next) {
- console.log("!!!!!!!!!");
- process.exit(1);
- if (!middleware) middleware = require('./router').middleware();
- middleware.call(this, next);
- }
-
- */
