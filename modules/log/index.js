1d7ab1c4702a03160628178609ba4a36387225a9
Ilya Kantor <iliakan@gmail.com>
2015-01-17 13:53:40 +0300

    LOG_ENV -> LOG_LEVEL

--- modules/log/index.js
+++ modules/log/index.js
@@ -18,7 +18,7 @@ var serializers = {
 };
 
 // we may want development logging in test mode
-var logEnv = process.env.LOG_ENV || process.env.NODE_ENV;
+var env = process.env.NODE_ENV;
 
 // if no name, then name is a parent module filename (or it's directory if index)
 // options.bufferLowLevel enables ring buffer for <= warn records
@@ -33,47 +33,58 @@ module.exports = function(name, options) {
   }
 
   var streams;
-  switch (logEnv) {
-  case 'development':
+
+  if (process.env.LOG_LEVEL) {
     streams = [
       {
-        level:  'debug',
+        level:  process.env.LOG_LEVEL,
         stream: process.stdout
       }
-    ];
-    break;
-  case 'test':
-    streams = [ /* don't log anything, set LOG_ENV if want to see errors */ ];
-    break;
-  case 'production':
-    // normally I see only info, but look in error in case of problems
-    streams = [
-      {
-        level:  'info',
-        stream: process.stdout
-      },
-      {
-        level:  'info',
-        stream: process.stderr
+    ]
+  } else {
+
+    switch (env) {
+    case 'development':
+      streams = [
+        {
+          level:  'debug',
+          stream: process.stdout
+        }
+      ];
+      break;
+    case 'test':
+      streams = [/* don't log anything, set LOG_ENV if want to see errors */];
+      break;
+    case 'production':
+      // normally I see only info, but look in error in case of problems
+      streams = [
+        {
+          level:  'info',
+          stream: process.stdout
+        },
+        {
+          level:  'info',
+          stream: process.stderr
+        }
+      ];
+
+      var RequestCaptureStream = require('requestCaptureStream');
+
+      // gather all data for req_id, but log only if warn happens
+      // ...and dump to stderr (in addition to
+      if (options.bufferLowLevel) {
+        streams.push({
+          level:  'debug',
+          type:   'raw',
+          stream: new RequestCaptureStream({
+            maxRecords:    150,
+            maxRequestIds: 2000,
+            dumpDefault:   true, // if error happens also dump all records, not bound to a request
+            // default records dumped AFTER request
+            stream:        process.stderr
+          })
+        });
       }
-    ];
-
-    var RequestCaptureStream = require('requestCaptureStream');
-
-    // gather all data for req_id, but log only if warn happens
-    // ...and dump to stderr (in addition to
-    if (options.bufferLowLevel) {
-      streams.push({
-        level:  'debug',
-        type:   'raw',
-        stream: new RequestCaptureStream({
-          maxRecords:    150,
-          maxRequestIds: 2000,
-          dumpDefault:   true, // if error happens also dump all records, not bound to a request
-          // default records dumped AFTER request
-          stream:        process.stderr
-        })
-      });
     }
   }
 
