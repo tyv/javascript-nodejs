4710f5418c524def0edab57095a2797da8b04353
Ilya Kantor <iliakan@gmail.com>
2015-01-20 14:22:11 +0300

    quiz minor model fixes

--- hmvc/quiz/models/quiz.js
+++ hmvc/quiz/models/quiz.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const config = require('config');
+const path = require('path');
 
 const schema = new Schema({
   title: {
@@ -11,9 +13,37 @@ const schema = new Schema({
     required: true
   },
   slug: {
-    type: String,
-    required: true
+    type:     String,
+    unique:   true,
+    required: true,
+    index:    true
   }
 });
+
+
+// all resources are here
+schema.statics.resourceFsRoot = path.join(config.publicRoot, 'quiz');
+
+schema.statics.getResourceFsRootBySlug = function(slug) {
+  return path.join(schema.statics.resourceFsRoot, slug);
+};
+
+schema.statics.getResourceWebRootBySlug = function(slug) {
+  return '/quiz/' + slug;
+};
+
+schema.statics.getUrlBySlug = function(slug) {
+  return '/' + slug;
+};
+
+schema.methods.getResourceFsRoot = function() {
+  return schema.statics.getResourceFsRootBySlug(this.get('slug'));
+};
+
+
+schema.methods.getResourceWebRoot = function() {
+  return schema.statics.getResourceWebRootBySlug(this.get('slug'));
+};
+
 
 module.exports = mongoose.model('Quiz', schema);
