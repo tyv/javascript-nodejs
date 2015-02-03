dd154235ae13193676b532033dd9c32cfe2a7fea
Ilya Kantor <iliakan@gmail.com>
2015-02-03 11:55:12 +0300

    add nginx password for prod

--- pm2/post_deploy.sh
+++ pm2/post_deploy.sh
@@ -6,5 +6,5 @@
 
 /usr/local/bin/pm2 startOrGracefulReload ecosystem.json --env production &&
  ./gulp cache:clean &&
- ./gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs/current &&
+ ./gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs/current --setPassword &&
  /etc/init.d/nginx reload
