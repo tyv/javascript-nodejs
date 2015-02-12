739dd02f14c8d427e49e9ac4abaabb9c3fec1f25
Ilya Kantor <iliakan@gmail.com>
2015-02-12 10:18:17 +0300

    up travis to 0.12

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -73,7 +73,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo /etc/init.d/mongodb restart
+sudo /etc/init.d/mongodb start
 
 # deploy nginx config
 sudo gulp config:nginx --prefix /etc/nginx --root /js/javascript-nodejs --env test --clear --harmony
