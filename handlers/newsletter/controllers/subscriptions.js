3e77be6edbac07ef5580a2ee4989e38aad001524
Ilya Kantor <iliakan@gmail.com>
2015-03-16 15:37:53 +0300

    subscription fixes, fixes #351

--- handlers/newsletter/controllers/subscriptions.js
+++ handlers/newsletter/controllers/subscriptions.js
@@ -13,35 +13,87 @@ exports.post = function*() {
     this.throw(404, "Нет такой рассылки");
   }
 
-  const subscription = new Subscription({
-    email:      this.request.body.email,
-    newsletter: newsletter._id
-  });
-
-
   if (this.req.user && this.req.user.email == this.request.email.slug) {
-    subscription.confirmed = true;
 
-    yield subscription.persist();
+    var existingSubscription = yield Subscription.findOne({
+      email:      this.request.body.email,
+      newsletter: newsletter
+    }).populate('newsletter').exec();
 
-    this.body = {
-      message: `Вы успешно подписаны, ждите писем на адрес ${this.request.email.slug}.`
-    };
+    if (existingSubscription) {
+      if (existingSubscription.confirmed) {
+        this.body = {
+          message: `Вы уже подписаны.`
+        };
+        return;
+      } else {
+        yield* sendConfirmationLetter.call(this, existingSubscription);
 
-  } else {
-    subscription.confirmed = false;
-    yield subscription.persist();
+        this.body = {
+          message: `Письмо-подтверждение отправлено заново на адрес ${existingSubscription.email}.`
+        };
+        return;
+      }
+    }
 
-
-    yield sendMail({
-      templatePath: path.join(this.templateDir, 'confirm-email'),
-      subject:      "Подтверждение: " + newsletter.title,
-      to:           this.request.body.email,
-      link:         (config.server.siteHost || 'http://javascript.in') + '/newsletter/confirm/' + subscription.accessKey
+    var subscription = yield Subscription.create({
+      email:      this.request.body.email,
+      newsletter: newsletter,
+      confirmed: true
     });
 
     this.body = {
-      message: "Проверьте почту " + this.request.body.email + " и подтвердите подписку, перейдя по ссылке в письме."
+      message: `Вы успешно подписаны, ждите писем на адрес ${subscription.email}.`
+    };
+
+  } else {
+    var existingSubscription = yield Subscription.findOne({
+      email:      this.request.body.email,
+      newsletter: newsletter
+    }).populate('newsletter').exec();
+
+    if (existingSubscription) {
+      if (existingSubscription.confirmed) {
+        this.body = {
+          message: `Вы уже подписаны.`
+        };
+        return;
+      } else {
+
+        console.log(existingSubscription);
+
+        yield* sendConfirmationLetter.call(this, existingSubscription);
+
+        this.body = {
+          message: `Письмо-подтверждение отправлено заново на адрес ${existingSubscription.email}.`
+        };
+        return;
+      }
+    }
+
+
+    var subscription = yield Subscription.create({
+      email:      this.request.body.email,
+      newsletter: newsletter,
+      confirmed: false
+    });
+
+    yield* sendConfirmationLetter.call(this, subscription);
+
+    this.body = {
+      message: `Проверьте почту ${subscription.email} и подтвердите подписку, перейдя по ссылке в письме.`
     };
   }
 };
+
+function* sendConfirmationLetter(subscription) {
+
+  yield sendMail({
+    templatePath: path.join(this.templateDir, 'confirm-email'),
+    newsletterTitle:   subscription.newsletter.title,
+    subject:      "Подтверждение: " + subscription.newsletter.title,
+    to:           subscription.email,
+    link:         (config.server.siteHost || 'http://javascript.in') + '/newsletter/confirm/' + subscription.accessKey
+  });
+
+}
