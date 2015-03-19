5a95fd879b87a342bbed40facaa71f245babe48a
Ilya Kantor <iliakan@gmail.com>
2015-03-19 19:19:20 +0300

    minor

--- pm2/post_deploy.sh
+++ pm2/post_deploy.sh
@@ -4,6 +4,7 @@
 # migrations
 # start
 
+/usr/local/bin/pm2 updatePM2
 /usr/local/bin/pm2 startOrGracefulReload ecosystem.json --env production &&
  gulp cache:clean --harmony &&
  gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs/current --setPassword --harmony --sslEnabled &&
