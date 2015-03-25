5a4d094ea8285cb33b86cd736c24ad0dfbed6bf0
Ilya Kantor <iliakan@gmail.com>
2015-03-25 12:10:57 +0300

    fix Firefox bug: on svg element className is not a string

--- client/head/unready.js
+++ client/head/unready.js
@@ -5,6 +5,8 @@
 document.addEventListener("click", function(event) {
   var target = event.target;
   while (target) {
+    if (!target.className.match) return; // click on SVG element in FF, className is an object
+
     if (target.className.match(/_unready\b/)) {
       event.preventDefault();
       return;
@@ -15,6 +17,8 @@ document.addEventListener("click", function(event) {
 
 // cancel submits of <form class="unready">
 document.addEventListener("submit", function(event) {
+  if (!event.target.className.match) return;
+
   if (event.target.className.match(/_unready\b/)) {
     event.preventDefault();
   }
