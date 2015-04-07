c55c90f08348250f6060aa885c91df402a4c5bbe
Ilya Kantor <iliakan@gmail.com>
2015-04-07 23:17:40 +0300

    minor fixes

--- handlers/payments/payanyway/controller/fail.js
+++ handlers/payments/payanyway/controller/fail.js
@@ -5,7 +5,8 @@ exports.get = function* (next) {
   yield* this.loadTransaction('MNT_TRANSACTION_ID');
 
   this.transaction.persist({
-    status: Transaction.STATUS_FAIL
+    status: Transaction.STATUS_FAIL,
+    statusMessage: 'оплата не прошла'
   });
 
   this.redirectToOrder();
--- handlers/payments/payanyway/router.js
+++ handlers/payments/payanyway/router.js
@@ -6,11 +6,13 @@ var callback = require('./controller/callback');
 
 var success = require('./controller/success');
 var cancel = require('./controller/cancel');
+var fail = require('./controller/fail');
 
 router.post('/callback', callback.post);
 
 router.all('/success', success.all);
 
 router.get('/cancel', cancel.get);
+router.get('/fail', fail.get);
 
 
