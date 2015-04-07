eb2729de1399a9edb04c963cf178d3d46ebd94e7
Ilya Kantor <iliakan@gmail.com>
2015-04-07 17:24:36 +0300

    minor

--- pm2/post_deploy.sh
+++ pm2/post_deploy.sh
@@ -7,5 +7,5 @@
 /usr/local/bin/pm2 updatePM2
 /usr/local/bin/pm2 startOrGracefulReload ecosystem.json --env production &&
  gulp cache:clean &&
- gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs/current --setPassword --sslEnabled &&
+ gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs/current --sslEnabled &&
  /etc/init.d/nginx reload
