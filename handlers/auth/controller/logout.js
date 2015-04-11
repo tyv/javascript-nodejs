da5006c105f7596eac8a2ccbd7bacf512fabb5ca
Ilya Kantor <iliakan@gmail.com>
2015-04-11 21:45:22 +0300

    logout deletes session, nginx improvements

--- handlers/auth/controller/logout.js
+++ handlers/auth/controller/logout.js
@@ -1,6 +1,11 @@
 
 exports.post = function*(next) {
   this.cookies.set('remember'); // remove "remember me" cookie
+  this.cookies.set('remember.sig');
+
+  this.cookies.set('sid'); // logout removes sid, but not sid.sig (3rd party bug?)
+  this.cookies.set('sid.sig');
+
   this.logout();
   this.session = null;
   this.redirect('/');
