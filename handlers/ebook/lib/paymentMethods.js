c45be31a16a1a4b8345b22cc78b78bb34fdef809
Ilya Kantor <iliakan@gmail.com>
2015-04-17 19:16:28 +0300

    limit payment methods for ebook

--- handlers/ebook/lib/paymentMethods.js
+++ handlers/ebook/lib/paymentMethods.js
@@ -3,16 +3,16 @@ const payments = require('payments');
 var paymentMethods = {};
 
 
-/*
-var methodsEnabled = ['webmoney', 'yandexmoney', 'payanyway', 'paypal'];
+
+var methodsEnabled = ['webmoney', 'yandexmoney', 'paypal', 'payanyway'];
 
 methodsEnabled.forEach(function(key) {
   paymentMethods[key] = payments.methods[key].info;
 });
-*/
 
+/*
 for (var name in payments.methods) {
   paymentMethods[name] = payments.methods[name].info;
 }
-
+*/
 module.exports = paymentMethods;
