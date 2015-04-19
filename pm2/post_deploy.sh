e2812e07e32b007204373c6d02dac48f6725bdbe
Ilya Kantor <iliakan@gmail.com>
2015-04-19 16:36:03 +0300

    typo

--- pm2/post_deploy.sh
+++ pm2/post_deploy.sh
@@ -13,6 +13,6 @@
 
 /usr/local/bin/pm2 startOrGracefulReload ecosystem.json --env production &&
   gulp cache:clean &&
-  gulp cloudfare:clean &&
+  gulp cloudflare:clean &&
   gulp config:nginx --prefix /etc/nginx --env production --root /js/javascript-nodejs/current --sslEnabled &&
   /etc/init.d/nginx reload
