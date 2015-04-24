209430494017006744c980cd9808d5c6d8c85389
Ilya Kantor <iliakan@gmail.com>
2015-04-24 23:59:21 +0300

    router with userById param

---
+++ modules/router.js
@@ -0,0 +1,32 @@
+var KoaRouter = require('koa-router');
+var inherits = require('inherits');
+var mongoose = require('mongoose');
+var User = require('users').User;
+
+function Router() {
+  KoaRouter.apply(this, arguments);
+
+  this.param('userById', function*(id, next) {
+
+    try {
+      new mongoose.Types.ObjectId(id);
+    } catch (e) {
+      // cast error (invalid id)
+      this.throw(404);
+    }
+
+    var user = yield User.findById(id).exec();
+
+    if (!user) {
+      this.throw(404);
+    }
+
+    this.params.userById = user;
+
+    yield* next;
+  });
+}
+
+inherits(Router, KoaRouter);
+
+module.exports = Router;
