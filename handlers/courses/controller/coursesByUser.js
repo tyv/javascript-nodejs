10f40c71de90388912b1a8c9ff806429c5c098c4
Ilya Kantor <iliakan@gmail.com>
2015-05-04 10:40:06 +0300

    courses page

---
+++ handlers/courses/controller/coursesByUser.js
@@ -0,0 +1,33 @@
+
+/**
+ * The order form is sent to checkout when it's 100% valid (client-side code validated it)
+ * It uses order.module.createOrderFromTemplate to create an order, it can throw if something's wrong
+ * the order CANNOT be changed after submitting to payment
+ * @param next
+ */
+exports.get = function*(next) {
+/*
+  var user = this.params.userById;
+
+  if (String(this.req.user._id) != String(user._id)) {
+    this.throw(403);
+  }
+
+  var orders = yield Order.find({
+    user: user._id,
+    status: {
+      $ne: Order.STATUS_CANCEL
+    }
+  }).sort({created: 1}).populate('user').exec();
+
+  var ordersToShow = [];
+
+  for (var i = 0; i < orders.length; i++) {
+    var format = require(orders[i].module).formatOrderForProfile;
+    if (!format) continue;
+    ordersToShow.push(yield* format.call(this, orders[i]));
+  }
+
+  this.body = ordersToShow;
+*/
+};
