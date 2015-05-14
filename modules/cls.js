833daa069b8dd1ebd83fdc43f4a3d9668ad61410
Ilya Kantor <iliakan@gmail.com>
2015-05-14 16:01:21 +0300

    minor

--- modules/cls.js
+++ modules/cls.js
@@ -1,8 +1,11 @@
+// This module initializes CLS
+// and throws in additional modules to integrate it w/ other libs if needed
+
 const clsNamespace = require("continuation-local-storage").createNamespace("app");
 
 // Must teach bluebird work with CLS
 // mz/fs uses bluebird by default if installed
-// something else installs bluebird
+// something else installs bluebird, so it gets used
 // if I don't teach bluebird here, it won't keep CLS context, then yield fs.stat will spoil context
 require('cls-bluebird')(clsNamespace);
 
