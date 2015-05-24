ba6b5d5bd66f8cfacd07c41024c2c4bb86c363e4
Ilya Kantor <iliakan@gmail.com>
2015-05-24 15:39:27 +0300

    add migration

---
+++ migrations/20150524153556-subscriptionSplitAction.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var co = require('co');
+var path = require('path');
+var gutil = require('gulp-util');
+var dataUtil = require('lib/dataUtil');
+var mongoose = require('lib/mongoose');
+var yargs = require('yargs');
+
+var Subscription = require('newsletter').Subscription;
+
+exports.up = function*() {
+
+  yield function(callback) {
+    Subscription.collection.remove({confirmed: false}, callback);
+  };
+
+  yield function(callback) {
+    Subscription.collection.update(
+        {confirmed: false},
+        { $unset: { confirmed: 1 } }, callback);
+  };
+
+};
+
+exports.down = function*() {
+  throw new Error("Rollback not implemented");
+};
