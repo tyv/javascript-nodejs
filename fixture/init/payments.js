0c5f29646c42ad551a2c80338e8e612a5c933cc0
Ilya Kantor <iliakan@gmail.com>
2015-05-25 19:30:01 +0300

    fixture

---
+++ fixture/init/payments.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+require('payments');
+
+exports.Transaction = [];
+exports.TransactionLog = [];
+exports.Order = [];
+
+exports.OrderTemplate = [
+  {
+    title:       "Язык JavaScript",
+    description: "600+ стр, PDF + EPUB (10Mb)",
+    slug:        "js",
+    module:      'ebook',
+    weight:      1,
+    amount:      1,
+    data:        {
+      file: "tutorial/js.zip"
+    }
+  },
+  {
+    title:       "Документ, события, интерфейсы",
+    description: "380+ стр, PDF + EPUB (8Mb)",
+    slug:        "ui",
+    module:      'ebook',
+    weight:      2,
+    amount:      1,
+    data:        {
+      file: "tutorial/ui.zip"
+    }
+  },
+  {
+    title:       "Две книги сразу",
+    description: "2xPDF + 2xEPUB, (18Mb)",
+    slug:        "js-ui",
+    module:      'ebook',
+    weight:      3,
+    amount:      1,
+    data:        {
+      file: "tutorial/js-ui.zip"
+    }
+  },
+  {
+    module: 'courses',
+    slug: 'course'
+  }
+];
+
