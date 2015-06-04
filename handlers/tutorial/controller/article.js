4163f622c1026abadf7be50972fb5d711ae102ad
Ilya Kantor <iliakan@gmail.com>
2015-06-05 00:00:24 +0300

    minor

--- handlers/tutorial/controller/article.js
+++ handlers/tutorial/controller/article.js
@@ -94,7 +94,7 @@ exports.get = function *get(next) {
   };
 
   locals.schema = {
-/*    itemtype: 'http://schema.org/Article',*/
+    itemtype: 'http://schema.org/WebPage'
   };
 
   this.body = this.render(renderedArticle.isFolder ? "folder" : "article", locals);
