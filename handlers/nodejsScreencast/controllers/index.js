3bd26aa916cccf6b514d782b52a84936dbb7a279
Ilya Kantor <iliakan@gmail.com>
2015-06-06 19:08:42 +0300

    minor

--- handlers/nodejsScreencast/controllers/index.js
+++ handlers/nodejsScreencast/controllers/index.js
@@ -2,7 +2,19 @@ var sendMail = require('mailer').send;
 var path = require('path');
 var config = require('config');
 
+var CourseGroup = require('courses').CourseGroup;
+var Course = require('courses').Course;
+
 exports.get = function*() {
   this.locals.siteToolbarCurrentSection = "nodejs-screencast";
+
+  var course = yield Course.findOne({slug: 'nodejs'}).exec();
+  if (course) {
+    var group = yield CourseGroup.findOne({course: course._id}).exec();
+    if (group) {
+      this.locals.hasOpenCourseGroups = true;
+    }
+  }
+
   this.body = this.render('index');
 };
