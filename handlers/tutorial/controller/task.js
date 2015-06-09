e03154b90852b78c410cc75139cf3c5d6510b4cb
Ilya Kantor <iliakan@gmail.com>
2015-06-09 23:27:11 +0300

    add task microdata breadcrumb

--- handlers/tutorial/controller/task.js
+++ handlers/tutorial/controller/task.js
@@ -1,5 +1,8 @@
+"use strict";
+
 const mongoose = require('mongoose');
 const Task = require('../models/task');
+const Article = require('../models/article');
 const TaskRenderer = require('../renderer/taskRenderer');
 
 exports.get = function *get(next) {
@@ -16,6 +19,29 @@ exports.get = function *get(next) {
   const renderer = new TaskRenderer();
 
   const rendered = yield* renderer.renderWithCache(task);
+
+  var breadcrumbs = [];
+
+  var parentId = task.parent._id;
+  while(true) {
+    let parent = yield Article.findById(parentId, {slug: 1, title: 1, parent: 1}).exec();
+    if (!parent) break;
+    breadcrumbs.push({
+      url: parent.getUrl(),
+      title: parent.title
+    });
+    parentId = parent.parent;
+  }
+  breadcrumbs.push({
+    title: 'Учебник',
+    url: '/'
+  });
+  breadcrumbs.push({
+    title: 'JavaScript.ru',
+    url: 'http://javascript.ru'
+  });
+
+  this.locals.breadcrumbs = breadcrumbs.reverse();
 
   this.locals.siteToolbarCurrentSection = "tutorial";
 
