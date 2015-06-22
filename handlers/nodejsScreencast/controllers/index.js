cbdc7748d344b9710677ce42fd095f9ca5b0279e
Ilya Kantor <iliakan@gmail.com>
2015-06-22 15:19:11 +0300

    add course signup on the screencast page

--- handlers/nodejsScreencast/controllers/index.js
+++ handlers/nodejsScreencast/controllers/index.js
@@ -4,17 +4,28 @@ var config = require('config');
 
 var CourseGroup = require('courses').CourseGroup;
 var Course = require('courses').Course;
+var money = require('money');
+var moment = require('momentWithLocale');
 
 exports.get = function*() {
   this.locals.siteToolbarCurrentSection = "nodejs-screencast";
 
+  this.locals.rateUsdRub = money.convert(1, {from: 'USD', to: 'RUB'});
+
   var course = yield Course.findOne({slug: 'nodejs'}).exec();
+  this.locals.groups = [];
   if (course) {
-    var group = yield CourseGroup.findOne({course: course._id}).exec();
-    if (group) {
-      this.locals.hasOpenCourseGroups = true;
-    }
+    this.locals.groups = yield CourseGroup.find({
+      course: course._id,
+      isOpenForSignup: true
+    }).sort({dateStart: 1}).exec();
   }
+
+  this.locals.formatGroupDate = function(date) {
+    return moment(date).format('D MMM YYYY').replace(/[а-я]/, function(letter) {
+      return letter.toUpperCase();
+    });
+  };
 
   this.body = this.render('index');
 };
