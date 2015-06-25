b36e0f01e0fdc7ced65eff69bc9f64b40d6adec8
Ilya Kantor <iliakan@gmail.com>
2015-06-25 09:50:26 +0300

    minor

--- handlers/ebook/lib/paymentMethods.js
+++ handlers/ebook/lib/paymentMethods.js
@@ -2,7 +2,7 @@ const payments = require('payments');
 
 var paymentMethods = {};
 
-var methodsEnabled = ['webmoney', 'yandexmoney', 'paypal', 'payanyway'];
+var methodsEnabled = ['webmoney', 'yandexmoney', 'paypal', 'payanyway', 'interkassa'];
 
 methodsEnabled.forEach(function(key) {
   paymentMethods[key] = payments.methods[key].info;
