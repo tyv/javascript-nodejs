7512bb320c98bca754b68dbd70ecdba4a348a50a
Ilya Kantor <iliakan@gmail.com>
2015-08-11 14:12:45 +0300

    fixes

---
+++ handlers/courses/controller/courseReviews.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const countries = require('countries');
+const CourseFeedback = require('../models/courseFeedback');
+const CourseGroup = require('../models/courseGroup');
+const Course = require('../models/course');
+const User = require('users').User;
+const _ = require('lodash');
+const CacheEntry = require('cache').CacheEntry;
+
+exports.get = function*() {
+
+  this.locals.course = yield Course.findOne({
+    slug: this.params.course
+  });
+
+  if (!this.locals.course) {
+    this.throw(404);
+  }
+
+  let type = this.accepts('json', 'html');
+
+  if (type == 'html') {
+
+    this.locals.title = "Отзыв о курсе\n" + this.locals.course.title;
+
+    // star => count
+    let feedbackStats = yield* CacheEntry.getOrGenerate({
+      key:  'courses:feedback:' + this.params.slug,
+      tags: ['courses:feedback']
+    }, getFeedbackStats.bind(this, this.locals.course));
+
+    this.body = this.render('feedback/list', {
+      stats: feedbackStats
+    });
+
+  }
+
+
+};
+
+
+function* getFeedbackStats(course) {
+
+  let groups = yield CourseGroup.find({
+    course: course.id
+  });
+
+  let groupIds = _.pluck(groups, '_id');
+
+  let stats = yield CourseFeedback.aggregate([
+    {
+      $match: {
+        group: {
+          $in: groupIds
+        }
+      }
+    },
+    {
+      $group: {
+        _id:   '$stars',
+        count: {
+          $sum: 1
+        }
+      }
+    }
+  ]).exec();
+
+  let totalFeedbacks = stats.reduce(function(prev, next) { return prev + next.count; }, 0);
+
+  console.log(totalFeedbacks);
+  // default stats (if no stars for a star)
+  let starStatsPopulated = {};
+  for(let i=1; i<=5; i++) starStatsPopulated[i] = {
+    count: 0,
+    fraction: 0
+  };
+
+  stats.forEach(function(stat) {
+    starStatsPopulated[stat._id] = {
+      count: stat.count,
+      fraction: stat.count ? +(stat.count / totalFeedbacks).toFixed(2) : 0
+    };
+  });
+
+
+  let recommendStats = yield CourseFeedback.aggregate([
+    {
+      $match: {
+        group: {
+          $in: groupIds
+        }
+      }
+    },
+    {
+      $group: {
+        _id:   '$recommend',
+        count: {
+          $sum: 1
+        }
+      }
+    }
+  ]).exec();
+
+
+  recommendStats = _.groupBy(recommendStats, '_id');
+
+  if (!recommendStats[true]) recommendStats[true] = [{count: 0}];
+  if (!recommendStats[false]) recommendStats[false] = [{count: 0}];
+
+  // 76% recommend
+  let recommendFraction = recommendStats[true][0].count / (recommendStats[true][0].count + recommendStats[false][0].count);
+
+  return {
+    stars: starStatsPopulated,
+    recommendFraction: recommendFraction,
+    total: totalFeedbacks
+  };
+}
