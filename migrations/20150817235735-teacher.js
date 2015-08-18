458d6c2afc36fded520ea486746f8673e8792db3
Ilya Kantor <iliakan@gmail.com>
2015-08-18 11:29:31 +0300

    add teacher

--- migrations/20150817235735-teacher.js
+++ migrations/20150817235735-teacher.js
@@ -5,9 +5,23 @@ var gutil = require('gulp-util');
 var dataUtil = require('lib/dataUtil');
 var mongoose = require('lib/mongoose');
 var yargs = require('yargs');
+var CourseGroup = require('courses').CourseGroup;
+var User = require('users').User;
 
 exports.up = function*() {
 
+  var ilya = yield User.findOne({
+    email: 'iliakan@gmail.com'
+  });
+
+  var groups = yield CourseGroup.find({});
+
+  for (var i = 0; i < groups.length; i++) {
+    var group = groups[i];
+    group.teacher = ilya._id;
+  }
+
+  yield group.save();
 };
 
 exports.down = function*() {
